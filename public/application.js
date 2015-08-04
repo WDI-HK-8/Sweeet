c60dc16a2f1ae49017318f67b7736f00e9f3c04f
Yohan <yohanology@gmail.com>
2015-08-04 23:25:08 +0800

    ft(spots): Add addSpot function + form on homepage when logged in

--- public/application.js
+++ public/application.js
@@ -77,6 +77,7 @@ $(document).ready(function(){
           $('#signin-form-button').addClass('hidden');
           $('#signup-form-button').addClass('hidden');
           $('#loggedin-email').removeClass('hidden');
+          $('#addspot-form-button').removeClass('hidden');
           console.log('authenticated',response)
         } else {
           console.log('not authenticated',response)
@@ -100,7 +101,50 @@ $(document).ready(function(){
     });
   }
 
+  var Spot = function(){
+
+  };
+
+  Spot.prototype.addSpot = function(details) {
+
+    $.ajax({
+      context: this,
+      type: 'POST',
+      url: '/spots',
+      data: {
+        spot: {
+          'name': details.name,
+          'type': details.type,
+          'address': details.address,
+          'district': details.district,
+          'city': details.city,
+          'wifitype': details.wifitype,
+          'wifispeed': details.wifispeed,
+          'wifipassword': details.wifipassword,
+          'price': details.price,
+          'outlets': details.outlets,
+          'seats': details.seats,
+          'food': details.food
+        }
+      },
+      success: function(response) {
+        console.log('place added',response);
+        $('.addspot-success').removeClass('hidden').text('Spot successfully added.');
+        
+        setTimeout(function(){
+          location.reload(true);
+        },1000);
+      },
+      error: function(response) {
+        $('.addspot-error').removeClass('hidden').text(response.responseText);
+      }
+    });
+  };
+
+
+
   var profile = new Profile();
+  var spot = new Spot();
 
   profile.checkSession();
 
@@ -129,5 +173,24 @@ $(document).ready(function(){
     profile.signOut();
   });
 
+  $('#addspot-form').submit(function(){
+    var details = {
+      'name': $('#addspot-name').val(),
+      'type': $('#addspot-type option:selected').text(),
+      'address': $('#addspot-address').val(),
+      'district': $('#addspot-district option:selected').text(),
+      'city': $('#addspot-city option:selected').text(),
+      'wifitype': $('#addspot-wifitype option:selected').text(),
+      'wifispeed': $('#addspot-wifispeed option:selected').text(),
+      'wifipassword': $('#addspot-wifipassword').val(),
+      'price': $('#addspot-price option:selected').text(),
+      'outlets': $('#addspot-outlets option:selected').text(),
+      'seats': $('#addspot-seats option:selected').text(),
+      'food': $('#addspot-food option:selected').text()
+    };
+
+    spot.addSpot(details);
+
+  });
 
 });
--- routes/spots.js
+++ routes/spots.js
@@ -20,16 +20,13 @@ exports.register = function(server,options,next) {
                 'address': request.payload.spot.address,
                 'district': request.payload.spot.district,
                 'city': request.payload.spot.city,
-                'wifi_type': request.payload.spot.wifitype,
-                'wifi_speed': request.payload.spot.wifispeed,
-                'wifi_password': request.payload.spot.wifipassword,
+                'wifitype': request.payload.spot.wifitype,
+                'wifispeed': request.payload.spot.wifispeed,
+                'wifipassword': request.payload.spot.wifipassword,
                 'price': request.payload.spot.price,
-                'outlets': request.payload.spot.outets,
+                'outlets': request.payload.spot.outlets,
                 'seats': request.payload.spot.seats,
                 'food': request.payload.spot.food,
-                'friendlyness': request.payload.spot.friendlyness,
-                'aircon': request.payload.spot.aircon,
-                'terrace':request.payload.spot.terrace,
                 'user_id': session.user_id
               }
               
@@ -51,11 +48,13 @@ exports.register = function(server,options,next) {
               address: Joi.string().required(),
               district: Joi.string().required(),
               city: Joi.string().required(),
-              wifi_type: Joi.string().required(),
-              wifi_speed: Joi.required(),
-              price: Joi.required(),
-              outlets: Joi.required(),
-              seats: Joi.required()
+              wifitype: Joi.string().required(),
+              wifispeed: Joi.string().required(),
+              wifipassword: Joi.string().allow(''),
+              price: Joi.string().required(),
+              outlets: Joi.string().required(),
+              seats: Joi.string().required(),
+              food: Joi.string().required()
             }
           }
         }
